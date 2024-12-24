a6a7ca543580f129e1583bcabe7a12c323e5d770
Dai Hung PHAM <daihung.pham@gmail.com>
2024-12-24 17:33:26 +0100

    aa

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 // src/auth/auth.service.ts
+
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseAuth } from '../config/firebase.config';
+import { firebaseAdminAuth } from '../config/firebase-admin.config';
 
 @Injectable()
 export class AuthService {
+    // User Login with Firebase Client SDK
     async login(email: string, password: string): Promise<string> {
         try {
             const userCredential = await signInWithEmailAndPassword(
@@ -13,16 +18,18 @@ export class AuthService {
             const token = await userCredential.user.getIdToken();
             return token;
         } catch (error) {
+            console.error(error);
             throw new UnauthorizedException('Invalid credentials');
         }
     }
 
+    // Verify JWT with Firebase Admin SDK
     async verifyToken(token: string): Promise<any> {
         try {
-            const decodedToken = await getAuth().verifyIdToken(token);
+            const decodedToken = await firebaseAdminAuth.verifyIdToken(token);
             return decodedToken;
         } catch (error) {
-            console.log('error verify Token');
+            console.error(error);
             throw new UnauthorizedException('Invalid token');
         }
     }
--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,5 +1,7 @@
 // src/config/firebase.config.ts
 
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { ConfigService } from '@nestjs/config';
 
 export const firebaseConfig = (configService: ConfigService) => ({
@@ -11,3 +13,14 @@ export const firebaseConfig = (configService: ConfigService) => ({
     databaseURL: configService.get<string>('FIREBASE_DATABASE_URL'),
     storageBucket: configService.get<string>('FIREBASE_STORAGE_BUCKET'),
 });
+
+const firebaseApp = initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+});
+
+export const firebaseAuth = getAuth(firebaseApp);
